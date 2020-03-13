525a62e59b139f5ebbb24b9d7b06442700309326
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-13 11:00:58 -0700

    axios updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,54 @@
 import React from 'react';
+
 import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
-
   state = {
-    users: []
+    users: [],
+    searchText: '',
+    followers: []
   };
 
   componentDidMount() {
+
+
     axios
-    .get('https://cors-anywhere.herokuapp.com/https://eddiemadrigal.net/data.js')
-    .then( res => {
-      console.log(res.data)
-    })
-    .catch(err => console.log(err))
+        .get(`https://api.github.com/users/eddiemadrigal/followers`)
+        .then(res => {
+          this.setState({
+            followers: res.data
+          });
+        })
+        .catch(err => console.log(err));
+
   }
+
+  
 
   render() {
     return (
-      <div>Test</div>
-    )
+      <div className="App">
+        <div>
+          
+
+          
+              <h2>Followers</h2>
+              
+                {this.state.followers.map( follower => (
+
+                  <div className="card-info" key={ follower.id }>
+                    <div>
+                      <h2>Name: { follower.name }</h2>
+                    </div>
+                  </div>
+
+                ))}
+
+        </div>
+      </div>
+    );
   }
-
-
 }
 
 export default App;
